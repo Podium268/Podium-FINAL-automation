ca4873c7679ccefcfdae07fe6f0523e00d37299d
zeldahyrule-bellaoni <hyrulezelda504@gmail.com>
2026-01-29 01:09:50 +0530

    Update rate-and-message-multiple-ladies.js

--- rate-and-message-multiple-ladies.js
+++ rate-and-message-multiple-ladies.js
@@ -7,9 +7,9 @@ module.exports = async function runRateAndMessageMultipleLadies(page, tierConfig
     // add all names you want to exclude
   ].map(n => n.toLowerCase())); // convert to lowercase for case-insensitive matching
 
-  const m1 = 'Hello';
-  const m2 = 'Hi'; //already won
-  const m3 = 'Hi'; //168
+  const m1 = 'A smile is a facelift that’s in everyone’s price range!	Big hugs and max stars 4U	(づ ◕‿◕ )づ';
+  const m2 = 'A smile is a facelift that’s in everyone’s price range!	Big hugs 4U	(づ ◕‿◕ )づ'; //already won
+  const m3 = 'A smile is a facelift that’s in everyone’s price range!	Big hugs 4U	(づ ◕‿◕ )づ'; //168
 
   const tabLabel = page._guid || 'T?'; //internal tab label in playwright
 
