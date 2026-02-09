70ca71eb9d050fe35a40991f5c09d14efd04b2a9
Podium268 <iqralatif268@gmail.com>
2026-02-10 00:38:01 +0500

    Update rate-and-message-multiple-ladies.js

--- rate-and-message-multiple-ladies.js
+++ rate-and-message-multiple-ladies.js
@@ -7,9 +7,9 @@ module.exports = async function runRateAndMessageMultipleLadies(page, tierConfig
     // add all names you want to exclude
   ].map(n => n.toLowerCase())); // convert to lowercase for case-insensitive matching
 
-  const m1 = 'Go on, eat the apple🍎, beauty.After that, let’s see whose name the mirror says.(Snow White reference)';
-  const m2 = 'Go on, eat the apple🍎, beauty.After that, let’s see whose name the mirror says.(Snow White reference)'; //already won
-  const m3 = 'Go on, eat the apple🍎, beauty.After that, let’s see whose name the mirror says.(Snow White reference)'; //168
+  const m1 = 'Skipping compliments today, just showering you with love and kisses.';
+  const m2 = 'Skipping compliments today, just showering you with love and kisses.'; //already won
+  const m3 = 'Skipping compliments today, just showering you with love and kisses.'; //168
 
   const tabLabel = page._guid || 'T?'; //internal tab label in playwright
 
