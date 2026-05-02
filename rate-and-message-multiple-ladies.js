58ceb19acecc23a30fb9bac73c8a2314c6560601
Podium268 <iqralatif268@gmail.com>
2026-05-02 09:05:42 +0500

    Update rate-and-message-multiple-ladies.js

--- rate-and-message-multiple-ladies.js
+++ rate-and-message-multiple-ladies.js
@@ -22,9 +22,9 @@ module.exports = async function runRateAndMessageMultipleLadies(page, tierConfig
     // add all names you want to exclude
   ].map(n => n.toLowerCase())); // convert to lowercase for case-insensitive matching
 
-  const m1 = 'Max stars for you my beautiful friend! ฅ^>⩊<^ ฅ';
-  const m2 = 'Max stars for you my beautiful friend! ฅ^>⩊<^ ฅ'; //already won
-  const m3 = 'Max stars for you my beautiful friend! ฅ^>⩊<^ ฅ'; //168
+  const m1 = 'Have A Blessed Friday Pretty Lady,max love(heart)';
+  const m2 = 'Have A Blessed Friday Pretty Lady,max love(heart)'; //already won
+  const m3 = 'Have A Blessed Friday Pretty Lady,max love(heart)'; //168
 
   const tabLabel = page._guid || 'T?'; //internal tab label in playwright
 
