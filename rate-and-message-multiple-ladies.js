a5358503a1e859c885cf5311ea82c9dd535e3d59
Podium268 <iqralatif268@gmail.com>
2026-05-15 17:17:52 +0500

    Update rate-and-message-multiple-ladies.js

--- rate-and-message-multiple-ladies.js
+++ rate-and-message-multiple-ladies.js
@@ -22,9 +22,9 @@ module.exports = async function runRateAndMessageMultipleLadies(page, tierConfig
     // add all names you want to exclude
   ].map(n => n.toLowerCase())); // convert to lowercase for case-insensitive matching
 
-  const m1 = 'It does not matter how slowly you go as long as you do not stop! Have A Great Day(heart)';
-  const m2 = 'It does not matter how slowly you go as long as you do not stop! Have A Great Day(heart)'; //already won
-  const m3 = 'It does not matter how slowly you go as long as you do not stop! Have A Great Day(heart)'; //168
+  const m1 = 'Have A Great Saturday(heart)';
+  const m2 = 'Have A Great Saturday(heart)'; //already won
+  const m3 = 'Have A Great Saturday(heart)'; //168
 
   const tabLabel = page._guid || 'T?'; //internal tab label in playwright
 
