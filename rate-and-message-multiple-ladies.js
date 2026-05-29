69082ee4b445983edd74b244d55a8fc769c97f6d
Podium268 <iqralatif268@gmail.com>
2026-05-30 00:08:43 +0500

    Update rate-and-message-multiple-ladies.js

--- rate-and-message-multiple-ladies.js
+++ rate-and-message-multiple-ladies.js
@@ -22,9 +22,9 @@ module.exports = async function runRateAndMessageMultipleLadies(page, tierConfig
     // add all names you want to exclude
   ].map(n => n.toLowerCase())); // convert to lowercase for case-insensitive matching
 
-  const m1 = 'The only way to do great work is to love what you do!(heart)';
-  const m2 = 'The only way to do great work is to love what you do!(heart)'; //already won
-  const m3 = 'The only way to do great work is to love what you do!(heart)'; //168
+  const m1 = 'Choose Happiness Every Day!(heart)';
+  const m2 = 'Choose Happiness Every Day!(heart)'; //already won
+  const m3 = 'Choose Happiness Every Day!(heart)'; //168
 
   const tabLabel = page._guid || 'T?'; //internal tab label in playwright
 
